bcda6aace304667cfcc163287b39865a4ff6a02f
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2025-12-11 08:50:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.5 MB (53.01%)

---
+++ my-solutions/0190-reverse-bits/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;
+            int lsb = n & 1;
+            result |= lsb;
+            n >>>= 1;
+        }
+        return result;
+    }
+}
+
