5bf66ffd41cb5d5055c506a5a543102ab3e0142f
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-02-10 00:12:38 +0000

    [LeetCode Sync] Runtime - 3 ms (94.55%), Memory - 76.8 MB (65.76%)

---
+++ my-solutions/1690-maximum-length-of-subarray-with-positive-product/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int getMaxLen(int[] nums) {
+        int pos=0, neg=0, ans=0;
+        for(int x: nums){
+            if(x==0){ pos=neg=0; }
+            else if(x>0){ pos++; neg = (neg==0)?0:neg+1; }
+            else { int t=pos; pos= (neg==0)?0:neg+1; neg=t+1; }
+            ans = Math.max(ans,pos);
+        }
+        return ans;
+
+    }
+}
