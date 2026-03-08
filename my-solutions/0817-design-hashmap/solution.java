777ea29448e8f004803a0114ff77414c10362046
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-03-08 17:11:56 +0000

    [LeetCode Sync] Runtime - 312 ms (5.21%), Memory - 243.5 MB (5.01%)

---
+++ my-solutions/0817-design-hashmap/solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class MyHashMap {
+    private int[] m;
+    public MyHashMap() {
+        m=new int[12345782];
+        Arrays.fill(m,-1);
+    }
+    
+    public void put(int key, int value) {
+        m[key]=value;
+    }
+    
+    public int get(int key) {
+        return m[key];
+    }
+    public void remove(int key) {
+        m[key]=-1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
