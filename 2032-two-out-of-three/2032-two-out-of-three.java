7dcea7f86f5e47ca1242950c245e664be3adbf7d
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-03-08 14:53:35 +0530

    Time: 5 ms (64.38%), Space: 47 MB (54.27%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public List<Integer> twoOutOfThree(int[] nums1, int[] nums2, int[] nums3) {
+        HashSet<Integer> s1=new HashSet<>();
+        HashSet<Integer> s2=new HashSet<>();
+        HashSet<Integer> s3=new HashSet<>();
+        HashSet<Integer> r=new HashSet<>();
+        for(int n:nums1) s1.add(n);
+        for(int n:nums2) s2.add(n);
+        for(int n:nums3) s3.add(n);
+        for(int n:s1){
+            if(s2.contains(n) || s3.contains(n)){
+                r.add(n);
+            }
+        }
+        for(int n:s2){
+            if(s3.contains(n)){
+                r.add(n);
+            }
+        }
+        return new ArrayList<>(r);
+    }
+}
