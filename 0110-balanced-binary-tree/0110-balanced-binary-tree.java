e23ad1c59e7e16b8a9e7f87333e65bd5ea0a7115
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-03-23 06:15:21 +0530

    Time: 0 ms (100%), Space: 45.5 MB (88.37%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        boolean[] t={true};
+        check(root,t);
+        return t[0];
+    }
+    private int check(TreeNode r,boolean[] t){
+        if(r==null){
+            return 0;
+        }
+        int lh=check(r.left,t);
+        int rh=check(r.right,t);
+
+        if(Math.abs(lh-rh)>1){
+            t[0]=false;
+        }
+        return Math.max(lh,rh)+1;
+    }
+}
