cbcbf0b801948aa5ffc14329feb4a11fd370eca9
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-03-24 16:02:32 +0000

    [LeetCode Sync] Runtime - 1 ms (98.31%), Memory - 44.9 MB (31.07%)

---
+++ my-solutions/3463-alternating-groups-i/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors) {
+        int count=0;
+        for(int i=0;i<colors.length;i++){
+            if((colors[i]!=colors[(i+1)%colors.length]) && (colors[(i+1)%colors.length]!=colors[(i+2)%colors.length])){
+                count++;
+            }
+        }
+        return count;
+    }
+}
