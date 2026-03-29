3a5d8c1115acab86d0462a871b1ef94080fedfeb
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-03-29 20:05:51 +0530

    Time: 1 ms (100%), Space: 44 MB (79.08%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        if (c1[0] != c2[0]) {
+            char temp = c1[0];
+            c1[0] = c1[2];
+            c1[2] = temp;
+        }
+        
+        if (c1[1] != c2[1]) {
+            char temp = c1[1];
+            c1[1] = c1[3];
+            c1[3] = temp;
+        }
+        return Arrays.equals(c1, c2);
+    }
+}
