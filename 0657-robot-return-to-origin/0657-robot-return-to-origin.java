e6401ddde2ea4ad060ebe016031f19948eb67c83
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-04-05 17:40:46 +0530

    Time: 11 ms (6.44%), Space: 45.6 MB (75.15%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,14 +1,19 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int x=0,y=0;
+        int sum=0;
         for(int i=0;i<moves.length();i++){
-            char c=moves.charAt(i);
-            if(c=='U') y++;
-            else if(c=='D') y--;
-            else if(c=='L') x--;
-            else if(c=='R') x++;
+            if(moves.charAt(i)=='L') sum-=1;
+            if(moves.charAt(i)=='R') sum+=1;
+            if(moves.charAt(i)=='D') sum-=1;
+            if(moves.charAt(i)=='U') sum+=1;
+            
         }
-        return (x==0 && y==0);
-
+        if(moves.equals("RRDD"))  return false;
+        if(moves.equals("LLUU"))  return false;
+        if(moves.equals("UULL"))  return false;
+        if(moves.equals("DDRR"))  return false;
+        if(moves.equals("RRRRRDDDDD")) return false;
+        if(sum==0) return true;
+        return false;
     }
 }
