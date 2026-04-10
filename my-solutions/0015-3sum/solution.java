cd73c0dbbfc2f6582d559c55749d49861a8282f5
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-04-10 15:46:12 +0000

    [LeetCode Sync] Runtime - 36 ms (30.40%), Memory - 60.3 MB (11.89%)

--- my-solutions/0015-3sum/solution.java
+++ my-solutions/0015-3sum/solution.java
@@ -1,40 +1,57 @@
+import java.util.*;
+
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
 
-        List<List<Integer>> l = new ArrayList<>();
-        int n = nums.length;
-
-        if (n < 3) return l;
-
+    public List<List<Integer>> kSum(int[] nums, int target, int k) {
         Arrays.sort(nums);
+        return kSumHelper(nums, target, k, 0);
+    }
 
-        int i = 0;
-        while (i < n - 2) {
+    private List<List<Integer>> kSumHelper(int[] nums, int target, int k, int start) {
+        List<List<Integer>> res = new ArrayList<>();
 
-            int j = i + 1;
-            int k = n - 1;
+        // 🔥 Base case: 2Sum
+        if (k == 2) {
+            int left = start, right = nums.length - 1;
 
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
+            while (left < right) {
+                int sum = nums[left] + nums[right];
 
-                if (sum == 0) {
-                    l.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    while (j < k && nums[k] == nums[k + 1]) k--;
+                if (sum == target) {
+                    res.add(Arrays.asList(nums[left], nums[right]));
 
-                } else if (sum < 0) {
-                    j++;
-                } else {
-                    k--;
-                }
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } 
+                else if (sum < target) left++;
+                else right--;
             }
-            i++;
-            while (i < n - 2 && nums[i] == nums[i - 1]) i++;
+            return res;
         }
 
-        return l;
+        // 🔥 Recursive case
+        for (int i = start; i <= nums.length - k; i++) {
+
+            if (i > start && nums[i] == nums[i - 1]) continue;
+
+            List<List<Integer>> temp = kSumHelper(nums, target - nums[i], k - 1, i + 1);
+
+            for (List<Integer> list : temp) {
+                List<Integer> newList = new ArrayList<>();
+                newList.add(nums[i]);
+                newList.addAll(list);
+                res.add(newList);
+            }
+        }
+
+        return res;
+    }
+
+    // ✅ 3Sum wrapper
+    public List<List<Integer>> threeSum(int[] nums) {
+        return kSum(nums, 0, 3);
     }
 }
-
