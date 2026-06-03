80fad4f260862e5e000c7e1d58d05a33f74ee4d2
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-06-03 16:55:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.8 MB (99.13%)

---
+++ my-solutions/2346-largest-3-same-digit-number-in-string/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String largestGoodInteger(String num) {
+        int max=Integer.MIN_VALUE;
+        String[] arr={"999","888","777","666","555","444","333","222","111","000"};
+        for(int i=0;i<arr.length;i++){
+            if(num.contains(arr[i])) return arr[i];
+        }
+        return "";
+    }
+}
