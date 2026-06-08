5ad1f753a69a300e180169d9077fc1a9bd301473
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-06-08 08:19:52 +0000

    [LeetCode Sync] Runtime - 4 ms (11.98%), Memory - 48.2 MB (90.34%)

---
+++ my-solutions/0958-sort-array-by-parity-ii/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int i=0,j=1;
+        while(i<nums.length && j<nums.length){
+            if(nums[i]%2==0){
+                i+=2;
+            }else if(nums[j]%2!=0){
+                j+=2;
+            }else{
+                nums[i]=nums[i]+nums[j];
+                nums[j]=nums[i]-nums[j];
+                nums[i]=nums[i]-nums[j];
+                i+=2;
+                j+=2;
+            }
+        }
+        return nums;
+    }
+}
