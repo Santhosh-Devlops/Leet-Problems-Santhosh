e44304ef7a9116e982a069b34a6b737b626bbd6d
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-06-16 03:44:21 +0000

    [LeetCode Sync] Runtime - 1 ms (98.20%), Memory - 43.5 MB (18.68%)

---
+++ my-solutions/3379-score-of-a-string/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int sum=0;
+        for(int i=0;i<s.length()-1;i++){
+            sum+=Math.abs((s.charAt(i)-'a')-(s.charAt(i+1)-'a'));
+        }
+        return sum;
+    }
+}
