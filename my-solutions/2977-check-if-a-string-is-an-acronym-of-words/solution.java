0e67e3a769b177d2d2df38d2c5a32860ce51a41a
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-06-18 08:49:22 +0000

    [LeetCode Sync] Runtime - 5 ms (19.78%), Memory - 46.7 MB (11.98%)

---
+++ my-solutions/2977-check-if-a-string-is-an-acronym-of-words/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAcronym(List<String> words, String s) 
+    {
+        if(words.size()!=s.length())
+        {
+            return false;
+        }
+        String b="";
+        for(String a:words)
+        {
+            b+=a.charAt(0);
+        }
+        if(b.equals(s)) return true;
+        return false;
+    }
+}
