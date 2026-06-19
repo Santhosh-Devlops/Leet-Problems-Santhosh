f5b4603b52c4dfb8ae7a4095b9e4c59a338ee9b2
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-06-19 10:20:22 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.1 MB (50.81%)

---
+++ my-solutions/1426-find-n-unique-integers-sum-up-to-zero/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int[] arr = new int[n];
+        int k = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            arr[k++] = i;
+            arr[k++] = -i;
+        }
+        
+        return arr;
+    }
+}
+
