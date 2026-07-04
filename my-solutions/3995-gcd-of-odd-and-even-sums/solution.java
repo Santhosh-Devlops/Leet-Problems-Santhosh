24c9fef805da7794a1acfeb13e1864d53fd45666
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-07-04 10:23:41 +0000

    [LeetCode Sync] Runtime - 1 ms (76.41%), Memory - 42.3 MB (95.21%)

---
+++ my-solutions/3995-gcd-of-odd-and-even-sums/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        /**int so=0,se=0;
+        for(int i=1;i<=n*2;i++){
+            if(i%2==0){
+                se+=i;
+            }
+            so+=i;
+        }
+        return gcd(se,so);**/
+        return n;
+    }
+}
