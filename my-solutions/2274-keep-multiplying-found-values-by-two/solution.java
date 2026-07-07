5a7a825efdb1b473f842ca11bbb5ada736b2e425
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-07-07 06:55:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45.3 MB (94.78%)

--- my-solutions/2274-keep-multiplying-found-values-by-two/solution.java
+++ my-solutions/2274-keep-multiplying-found-values-by-two/solution.java
@@ -1,21 +1,11 @@
 class Solution {
     public int findFinalValue(int[] nums, int original) {
-
-        int k = original;
-        boolean flag = true;
-
-        while (flag) {
-            flag = false;
-
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == k) {
-                    k = k * 2;
-                    flag = true;
-                    break;
-                }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==original){
+               return findFinalValue(nums,original*2);
             }
+            
         }
-        return k;
+        return original;
     }
 }
-
