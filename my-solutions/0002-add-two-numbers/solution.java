9a9fe85cc72df411e3a17ac6932b0f8fefaa3e4a
Santhosh-Devlops <santhoshkumark7680@gmail.com>
2026-07-11 01:22:35 +0000

    [LeetCode Sync] Runtime - 11 ms (1.85%), Memory - 47.5 MB (6.72%)

--- my-solutions/0002-add-two-numbers/solution.java
+++ my-solutions/0002-add-two-numbers/solution.java
@@ -8,28 +8,51 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ import java.math.BigInteger;
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode l3=new ListNode();
-        ListNode finals=l3;
-        int carry=0,sum=0;
-        while(l1!=null||l2!=null||carry!=0){
-            sum=carry;
-            if(l1!=null){
-                sum+=l1.val;
-                l1=l1.next;
-                System.out.println(sum);
-            }
-            if(l2!=null){
-                sum+=l2.val;
-                l2=l2.next;
-                System.out.println(sum);
-            }
-            int o=sum%10;
-            carry=sum/10;
-            l3.next=new ListNode(o);
-            l3=l3.next;
+        if(l1==null && l2==null){
+            return null;
         }
-        return finals.next;
+        ListNode dummy=null;
+        ListNode curr=null;
+        String n1="",n2="";
+        while(l1!=null){
+            n1+=String.valueOf(l1.val);
+            l1=l1.next;
+        }
+        
+        String s1=new StringBuilder(n1).reverse().toString();
+
+        while(l2!=null){
+            n2+=String.valueOf(l2.val);
+            l2=l2.next;
+        }
+        String s2=new StringBuilder(n2).reverse().toString();
+        
+        BigInteger o1=new BigInteger(s1);
+        BigInteger o2=new BigInteger(s2);
+        BigInteger sum=o1.add(o2);
+        if (sum.equals(BigInteger.ZERO)) {
+            return new ListNode(0);
+        }
+
+        while(!sum.equals(BigInteger.ZERO)){
+            BigInteger[] dr=sum.divideAndRemainder(BigInteger.TEN); 
+            int r=dr[1].intValue();
+            sum=dr[0];
+            if(dummy==null) {
+                dummy=new ListNode(r);
+                curr=dummy;
+            }
+            else{
+                ListNode ne=new ListNode(r);
+                curr.next=ne;
+                curr=ne;
+            }
+            
+        }
+        return dummy;
     }
 }
+
